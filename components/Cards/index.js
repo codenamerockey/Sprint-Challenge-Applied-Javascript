457b9f851a93f7dff9b5111614b45a8fc0dfc3d2
Rockey Simmons <codenamerockey@gmail.com>
2019-08-09 10:21:48 -0700

    created proper documentation for each section and also cleaned up my header and tabs js file with more stream line code

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,75 +18,92 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
+/**Code Starts Here**/
+
+//Getting the data from the server endpoint below
 let articleAPI = axios.get(
   'https://lambda-times-backend.herokuapp.com/articles'
 );
 
+//Getting a handle on the section I need to append the tabs too.
 const cardsContainer = document.querySelector('.cards-container');
 
+//Function created for the card component;
 function articleCard(obj) {
+  //create div container element for all other elements to sit inside of.
   const divCard = document.createElement('div');
   divCard.classList.add('card');
 
+  //create div container  element for headline
   const headline = document.createElement('div');
   headline.classList.add('headline');
-  headline.textContent = `${obj.headline}`;
+  headline.textContent = `${obj.headline}`; // populates the data from the server when the object is passed as an argument to the function
   divCard.appendChild(headline);
 
+  //create div container for author section
   const authorDiv = document.createElement('div');
   authorDiv.classList.add('author');
   divCard.appendChild(authorDiv);
 
+  // create div container for author image
   const imgDiv = document.createElement('div');
   imgDiv.classList.add('img-container');
   authorDiv.appendChild(imgDiv);
 
+  // create image element for author photo
   const image = document.createElement('img');
-  image.setAttribute('src', `${obj.authorPhoto}`);
+  image.setAttribute('src', `${obj.authorPhoto}`); // populates the data from the server when the object is passed as an argument to the function
+  image.setAttribute('alt', 'picture of the author');
   imgDiv.appendChild(image);
 
+  // create span element to hold authors name.
   const authorSpan = document.createElement('span');
-  authorSpan.textContent = `By ${obj.authorName}`;
+  authorSpan.textContent = `By ${obj.authorName}`; // populates the data from the server when the object is passed as an argument to the function
   authorDiv.appendChild(authorSpan);
 
+  //return the entire div with all the elements created inside when function is invoked
   return divCard;
 }
 
-console.log(
-  articleAPI.then(response => {
-    const js = response.data.articles.javascript;
-    console.log(js);
-    js.forEach(article => {
-      cardsContainer.appendChild(articleCard(article));
-    });
+//Using the returned promise from the api to loop through each article object/array and also append the function that creates the cards to the Dom container holding it. Within that function the object is being passed as an argument to the function, which is populating the data, based on the textContent fields and src attribute inside the function.
+articleAPI.then(response => {
+  //Individual object data structure
+  const js = response.data.articles.javascript;
+  console.log(js);
+  js.forEach(article => {
+    cardsContainer.appendChild(articleCard(article)); //card being appended to the DOM
+  });
 
-    const bs = response.data.articles.bootstrap;
-    console.log(bs);
-    bs.forEach(article => {
-      cardsContainer.appendChild(articleCard(article));
-    });
+  //Individual object data structure
+  const bs = response.data.articles.bootstrap;
+  console.log(bs);
+  bs.forEach(article => {
+    cardsContainer.appendChild(articleCard(article)); //card being appended to the DOM
+  });
 
-    const tech = response.data.articles.technology;
-    console.log(tech);
-    tech.forEach(article => {
-      cardsContainer.appendChild(articleCard(article));
-    });
+  //Individual object data structure
+  const tech = response.data.articles.technology;
+  console.log(tech);
+  tech.forEach(article => {
+    cardsContainer.appendChild(articleCard(article)); //card being appended to the DOM
+  });
 
-    const jq = response.data.articles.jquery;
-    console.log(jq);
-    jq.forEach(article => {
-      cardsContainer.appendChild(articleCard(article));
-    });
+  //Individual object data structure
+  const jq = response.data.articles.jquery;
+  console.log(jq);
+  jq.forEach(article => {
+    cardsContainer.appendChild(articleCard(article)); //card being appended to the DOM
+  });
 
-    const node = response.data.articles.node;
-    console.log(node);
-    node.forEach(article => {
-      cardsContainer.appendChild(articleCard(article));
-    });
-    // console.log(
-    //   js.forEach(object => {
-    //     console.log(object.headline);
-    //   })
-    // );
-  })
-);
+  //Individual object data structure
+  const node = response.data.articles.node;
+  console.log(node);
+  node.forEach(article => {
+    cardsContainer.appendChild(articleCard(article)); //card being appended to the DOM
+  });
+  // console.log(
+  //   js.forEach(object => {
+  //     console.log(object.headline);
+  //   })
+  // );
+});
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,29 +9,38 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-// created handle to headerContainer element inside index.html
+/** Code Starts Here.... **/
+
+// created handle to headerContainer AKA (header-container) element inside index.html
 const headerContainer = document.querySelector('.header-container');
 
+//Function created for the header component
 function header() {
+  //create div container for all other elements to sit inside of.
   const divHeader = document.createElement('div');
   divHeader.classList.add('header');
   headerContainer.appendChild(divHeader);
 
+  //create span element to hold the date.
   const spanDate = document.createElement('span');
   spanDate.classList.add('date');
   spanDate.textContent = 'SMARCH 28, 2019';
   divHeader.appendChild(spanDate);
 
+  //create h1 element for the Title of the page
   const headerOne = document.createElement('h1');
   headerOne.textContent = 'Lambda Times';
   divHeader.appendChild(headerOne);
 
+  //create a span tag the will hold the temperature with the html entity.
   const spanTemp = document.createElement('span');
   spanTemp.classList.add('temp');
   spanTemp.textContent = '98°';
   divHeader.appendChild(spanTemp);
 
+  //return the completed div with all elements created inside when function is invoked.
   return divHeader;
 }
 
+//This line of code appends the div created inside the function named header() and it is appended to the parent container.
 headerContainer.appendChild(header());
